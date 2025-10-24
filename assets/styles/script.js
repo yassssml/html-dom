d5802741b88c2f3fc10385b4a874bf40835cc93b
Yasmin <ycrisostomo051@gmail.com>
2025-10-24 15:49:06 -0300

    exemplo 2

--- assets/styles/script.js
+++ assets/styles/script.js
@@ -1,20 +1,25 @@
-const botao = document.getElementById("botao");
 const titulo = document.getElementById("titulo");
-const body = document.body;
+const descricao = document.getElementById("descricao");
+const imagem = document.getElementById("imagem");
 
+const btnPraia = document.getElementById("btnPraia");
+const btnMontanha = document.getElementById("btnMontanha");
+const btnCidade = document.getElementById("btnCidade");
 
-botao.addEventListener("click", () => {
-
-  if (body.classList.contains("claro")) {
-   
-    body.classList.replace("claro", "escuro");
-    titulo.textContent = "Modo Escuro 🌙";
-    botao.textContent = "Ativar modo claro 🌞";
-  } else {
-
-    body.classList.replace("escuro", "claro");
-    titulo.textContent = "Modo Claro 🌞";
-    botao.textContent = "Ativar modo escuro 🌙";
-  }
+btnPraia.addEventListener("click", () => {
+  titulo.textContent = "Praia 🏖️";
+  descricao.textContent = "Você está em uma praia ensolarada, sentindo a brisa do mar.";
+  imagem.src = "assets/img/praia.avif";
 });
 
+btnMontanha.addEventListener("click", () => {
+  titulo.textContent = "Montanha 🏔️";
+  descricao.textContent = "Você subiu até o topo e sente o ar puro da montanha.";
+  imagem.src = "assets/img/montanha.jpg";
+});
+
+btnCidade.addEventListener("click", () => {
+  titulo.textContent = "Cidade 🌆";
+  descricao.textContent = "As luzes da cidade brilham enquanto a noite chega.";
+  imagem.src = "assets/img/cidade.jpg";
+});
